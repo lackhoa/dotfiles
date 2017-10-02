80413096cd61ce2f294e48e830c08d0c6de35263
khoa <vodangkhoa873@gmail.com>
2017-10-02 21:03:51 +0300

    Fixed directory issue for khoa_motd

--- bin/khoa_motd.sh
+++ bin/khoa_motd.sh
@@ -3,6 +3,6 @@
 #This script will read from a random line in khoa_motd.txt
 # Kind of like message of the day
 
-LINE_NUM=`wc -l < khoa_motd.txt`
-RAN_LINE=$(( ($RANDOM % LINE_NUM) + 1))
-gvim +$RAN_LINE khoa_motd.txt
+LINE_NUM=`wc -l < ~/bin/khoa_motd.txt`
+RAN_LINE=$(( ($RANDOM % $LINE_NUM) + 1))
+gvim +$RAN_LINE ~/bin/khoa_motd.txt
