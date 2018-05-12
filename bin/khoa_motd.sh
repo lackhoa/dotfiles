c8691b179a26671d1b0ab5a72a512bc3a5aa6380
khoa <vodangkhoa873@gmail.com>
2018-05-12 10:59:02 +0300

    Fixed the motd code

--- bin/khoa_motd.sh
+++ bin/khoa_motd.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-#This script will read from a random line in khoa_motd.txt
+#This script will read from a random line in k_motd.txt
 # Kind of like message of the day
 
-LINE_NUM=`wc -l < ~/bin/khoa_motd.txt`
+LINE_NUM=`wc -l < ~/bin/k_motd.txt`
 RAN_LINE=$(( ($RANDOM % $LINE_NUM) + 1))
 gvim +$RAN_LINE ~/bin/k_motd.txt
