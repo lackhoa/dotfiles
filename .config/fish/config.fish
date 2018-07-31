9c7a98a40e3daa96039a5fa7ed9244281a0c01c2
khoa <vodangkhoa873@gmail.com>
2018-07-31 20:24:03 +0300

    Switch to emacs GUI

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 abbr cls 'clear'
 abbr desktop 'cdl ~/Desktop'
 abbr subl '/usr/bin/subl'
-abbr edit-abbr 'emacs -nw ~/.config/fish/config.fish'
+abbr edit-abbr 'emacs ~/.config/fish/config.fish'
 abbr update-abbr 'source ~/.config/fish/config.fish'
 abbr cp 'cp -vi'
 abbr mv 'mv -vi'
@@ -9,13 +9,12 @@ abbr cdw 'cdl ~/Work'
 abbr cdd 'cdl ~/Downloads'
 abbr cdc 'cdl ~/Coq'
 abbr x 'xset dpms force off'
-abbr emacs 'emacs -nw'
 abbr gs 'git status'
 abbr grep 'grep -C 10'
 
 # clean up stuff that were created less than 1 minute ago
 abbr clean 'find . -type f -cmin -1 -delete'
-abbr conky-conf 'sudo emacs -nw /etc/conky/conky.conf'
+abbr conky-conf 'sudo emacs /etc/conky/conky.conf'
 
 # this does everything I want to do about updating
 # this command is stored in the root user's script path
