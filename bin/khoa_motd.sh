6396da53cb9c081d881cd162cb8ba996d21366fa
lackhoa <vodangkhoa873@gmail.com>
2019-04-29 00:20:09 +0300

    motd

--- bin/khoa_motd.sh
+++ bin/khoa_motd.sh
@@ -3,6 +3,7 @@
 #This script will read from a random line in k_motd.txt
 # Kind of like message of the day
 
-LINE_NUM=`wc -l < ~/bin/k_motd.txt`
+file = "~/note/thought.txt"
+LINE_NUM=`wc -l < ~/note/thought.txt`
 RAN_LINE=$(( ($RANDOM % $LINE_NUM) + 1))
 emacs +$RAN_LINE ~/note/thought.txt
