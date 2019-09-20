8ca943ec4f19899c243526c92df4fc8c38cae35c
lackhoa <vodangkhoa873@gmail.com>
2019-09-21 01:20:50 +0300

    Focus on hiding s-exp

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,5 @@
-(defun config () (interactive) (find-file "~/.emacs.d/init.el"))
-(defun thought () (interactive) (find-file "~/note/thought.txt"))
-
 (require 'package)
 
-;; Version 27.0 automatically initializes packages for you
 (when (version< emacs-version "27.0")
   (package-initialize))
 
@@ -18,49 +14,39 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; This is only needed once, near the top of the file
 (eval-when-compile
   (require 'use-package))
 
-;; Ignore case in minibuffer's tab completion
-(setq completion-ignore-case t
+
+(setq completion-ignore-case t ; Ignore case in minibuffer's tab completion
       read-file-name-completion-ignore-case t
       read-buffer-completion-ignore-case t)
 
-(;; Windows redoer
- winner-mode 1)
+(winner-mode 1)  ; Windows redoer
 
-(;; Don't skip the screen when scrolling up or down
- setq scroll-conservatively 100)
+(setq scroll-conservatively 100)  ; Don't skip the screen when scrolling up or down
 
-(;; Something really annoying?
- setq ring-bell-function 'ignore)
+(setq ring-bell-function 'ignore)  ; Something really annoying?
 
-(;; Show where the cursor is
- global-hl-line-mode)
+(global-hl-line-mode)  ; Show where the cursor is
 
-;;; Getting rid of really annoying buffers
-;; Removes *messages* buffer.
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
+(progn  ; Getting rid of really annoying buffers
+  (setq-default message-log-max nil)
+  (kill-buffer "*Messages*")
+  (setq inhibit-startup-buffer-menu t)
+  (setq inhibit-startup-screen t))
 
-(;; Don't show *Buffer list* when opening multiple files at the same time.
- setq inhibit-startup-buffer-menu t)
-(;; Don't show Welcome Screen when opening up
- setq inhibit-startup-screen t)
+(fset 'yes-or-no-p 'y-or-n-p)  ;; No more typing the whole yes or no
 
-(;; No more typing the whole yes or no. Just y or n will do.
- fset 'yes-or-no-p 'y-or-n-p)
+(setq auto-mode-alist  ; Bind file extension to modes
+      (append '(("\\.rkt\\'" . scheme-mode)
+                ("\\.pl$"    . prolog-mode))
+              auto-mode-alist))
 
-(;; Adding new file extension to modes
- setq auto-mode-alist (append '(("\\.rkt\\'" . scheme-mode)
-                                ("\\.pl$"    . prolog-mode))
-                              auto-mode-alist))
-
-;; Get rid of UI elements
-(menu-bar-mode     -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode     -1)
+(progn  ;; Get rid of UI elements
+  (menu-bar-mode     -1)
+  (toggle-scroll-bar -1)
+  (tool-bar-mode     -1))
 
 (;; Auto-pair
  electric-pair-mode)
@@ -81,7 +67,6 @@
   (add-hook 'evil-normal-state-entry-hook
             '(lambda () (global-hl-line-mode)))
 
-  ;; Surround
   (use-package evil-surround
     :ensure t
     :config (global-evil-surround-mode))
@@ -95,53 +80,47 @@
     (evil-scroll-line-to-center (line-number-at-pos)))
 
   (use-package evil-commentary
-    ;; Commentary
     :ensure t
     :config (evil-commentary-mode)))
 
+(progn  ;;; Key bindings
+  (;; No more M-x! Use smex instead of evil-ex
+   evil-define-key 'normal 'global ";" 'smex)
+  (evil-define-key 'normal 'global "a" 'evil-append-line)
+  (evil-define-key 'normal 'global "A" 'evil-append)
+  (evil-define-key 'normal 'global "p" 'evil-paste-before)
+  (evil-define-key 'normal 'global "P" 'evil-paste-after)
+  (evil-define-key 'normal 'global "W" 'forward-sexp)
+  (evil-define-key 'normal 'global "B" 'backward-sexp)
+  (evil-define-key 'normal 'global (kbd "<up>") 'evil-scroll-line-up)
+  (evil-define-key 'normal 'global (kbd "<down>") 'evil-scroll-line-down)
+  (evil-define-key 'normal 'global (kbd "<left>") 'previous-buffer)
+  (evil-define-key 'normal 'global (kbd "<right>") 'next-buffer)
+  (evil-define-key 'normal 'global (kbd "RET") 'evil-write-all)
+  (evil-define-key 'normal 'global (kbd "K") 'open-line)
+  (evil-define-key 'normal 'global (kbd "SPC") (lambda () (interactive)
+  (insert-char ?\s)
+  (evil-backward-char)))
+  (evil-define-key 'normal 'global (kbd "C-j") (lambda () (interactive)
+  (save-excursion
+  (end-of-line)
+  (open-line 1))))
+  (evil-define-key 'normal 'global (kbd "C-k") (lambda () (interactive)
+  (save-excursion
+  (end-of-line 0)
+  (open-line 1))))
+  (evil-define-key 'normal 'global (kbd "C-a") 'mark-whole-buffer)
+  (evil-define-key 'normal 'global (kbd "DEL") 'backward-delete-char-untabify)
+  (evil-define-key 'normal 'global (kbd "C-.") 'next-buffer)
+  (evil-define-key 'normal 'global (kbd "C-,") 'previous-buffer)
+  (evil-define-key 'normal 'global (kbd "\\")  (lambda () (interactive) (message "Want Enter?")))
 
+  (evil-define-key 'insert 'global (kbd "C-.") 'next-buffer)
+  (evil-define-key 'insert 'global (kbd "C-,") 'previous-buffer)
+  (evil-define-key 'insert 'global (kbd "C-v") 'evil-paste-before)
 
-
-
-;;; Key bindings
-(progn
- (;; No more M-x! Use smex instead of evil-ex
-  evil-define-key 'normal 'global ";" 'smex)
- (evil-define-key 'normal 'global "a" 'evil-append-line)
- (evil-define-key 'normal 'global "A" 'evil-append)
- (evil-define-key 'normal 'global "p" 'evil-paste-before)
- (evil-define-key 'normal 'global "P" 'evil-paste-after)
- (evil-define-key 'normal 'global "W" 'forward-sexp)
- (evil-define-key 'normal 'global "B" 'backward-sexp)
- (evil-define-key 'normal 'global (kbd "<up>") 'evil-scroll-line-up)
- (evil-define-key 'normal 'global (kbd "<down>") 'evil-scroll-line-down)
- (evil-define-key 'normal 'global (kbd "<left>") 'previous-buffer)
- (evil-define-key 'normal 'global (kbd "<right>") 'next-buffer)
- (evil-define-key 'normal 'global (kbd "RET") 'evil-write-all)
- (evil-define-key 'normal 'global (kbd "K") 'open-line)
- (evil-define-key 'normal 'global (kbd "SPC") (lambda () (interactive)
-                                                (insert-char ?\s)
-                                                (evil-backward-char)))
- (evil-define-key 'normal 'global (kbd "C-j") (lambda () (interactive)
-                                                (save-excursion
-                                                  (end-of-line)
-                                                  (open-line 1))))
- (evil-define-key 'normal 'global (kbd "C-k") (lambda () (interactive)
-                                                (save-excursion
-                                                  (end-of-line 0)
-                                                  (open-line 1))))
- (evil-define-key 'normal 'global (kbd "C-a") 'mark-whole-buffer)
- (evil-define-key 'normal 'global (kbd "DEL") 'backward-delete-char-untabify)
- (evil-define-key 'normal 'global (kbd "C-.") 'next-buffer)
- (evil-define-key 'normal 'global (kbd "C-,") 'previous-buffer)
- (evil-define-key 'normal 'global (kbd "\\")  (lambda () (interactive) (message "Want Enter?")))
-
- (evil-define-key 'insert 'global (kbd "C-.") 'next-buffer)
- (evil-define-key 'insert 'global (kbd "C-,") 'previous-buffer)
- (evil-define-key 'insert 'global (kbd "C-v") 'evil-paste-before)
-
- (evil-define-key 'visual 'global (kbd "TAB") 'indent-rigidly)
- (evil-define-key 'visual 'global (kbd ";")   'smex))
+  (evil-define-key 'visual 'global (kbd "TAB") 'indent-rigidly)
+  (evil-define-key 'visual 'global (kbd ";")   'smex))
 
 ;;; Some vital command alias
 (defalias 'k 'kill-buffer-and-window)
@@ -159,32 +138,25 @@
               evil-visual-state-map
               evil-insert-state-map)))
 
-
 (defun disable-all-themes ()
-  "disable all active themes."
   (dolist (i custom-enabled-themes)
     (disable-theme i)))
 
-(use-package evil-vimish-fold
-  :ensure t
-  :config (evil-vimish-fold-mode 1))
-
-(use-package avy
-  ;; The dopest snipe package ever
+(use-package fold-this  ; Just hide the region!
+  ;; Unfold with "enter"
   :ensure t
   :config
+  (fold-this-mode 1)
+  (evil-define-key 'normal 'global (kbd "H") #'fold-this-sexp)
+  (evil-define-key 'visual 'global (kbd "H") #'fold-this-sexp))
+
+(use-package avy  ; The dopest snipe package ever
+  :ensure t
+  :config
+  (evil-define-key 'normal 'global (kbd "f") #'avy-goto-char)
   (evil-define-key 'normal 'global (kbd "s") #'avy-goto-char-2))
 
-;; (;; Why even bother with the default status bar?
-;;  use-package spaceline
-;;  :ensure t
-;;  :init (setq powerline-default-separator 'arrow)
-;;  :config
-;;  (require 'spaceline-config)
-;;  (spaceline-spacemacs-theme))
-
-(use-package ido-vertical-mode
-  ;; Ido-mode: a regexp smart search framework
+(use-package ido-vertical-mode ; Ido-mode: a regexp smart search framework
   :ensure t
   :init
   (ido-mode 1)
@@ -197,8 +169,7 @@
   (ido-vertical-mode 1)
   (setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right))
 
-(use-package smex
-  ;; Command completion, using Ido above
+(use-package smex  ; Command completion, using Ido above
   :ensure t
   :init (smex-initialize)
   :bind ("M-x" . smex))
@@ -213,93 +184,67 @@
   :ensure t
   :hook ((prog-mode) . aggressive-indent-mode))
 
-(use-package linum-relative
-  ;; Relative line number
+(column-number-mode 1)  ; Show columns
+
+(progn  ; List of buffers to not open in a new window
+  (add-to-list 'same-window-buffer-names "*Buffer List*")
+  (add-to-list 'same-window-buffer-names "*Help*")
+  (add-to-list 'same-window-buffer-names "*Quail Completions*"))
+
+(use-package rainbow-delimiters  ; Color those parentheses
   :ensure t
-  :init
-  (setq linum-relative-current-symbol "")
-  (linum-relative-global-mode))
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'text-mode-hook 'rainbow-delimiters-mode))
 
-(;; Show columns
- column-number-mode 1)
+(use-package rainbow-identifiers  ; Color those identifiers
+  :ensure t
+  :config
+  (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
+  (add-hook 'text-mode-hook 'rainbow-identifiers-mode))
 
-;; List of buffers to not open in a new window
-(add-to-list 'same-window-buffer-names "*Buffer List*")
-(add-to-list 'same-window-buffer-names "*Help*")
-
-(;; Color those parentheses
- use-package rainbow-delimiters
- :ensure t
- :config
- (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
- (add-hook 'text-mode-hook 'rainbow-delimiters-mode))
-
-(;; Color those identifiers
- use-package rainbow-identifiers
- :ensure t
- :config
- (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
- (add-hook 'text-mode-hook 'rainbow-identifiers-mode))
-
-(;; No tabs!
- setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode nil)  ; No tabs!
 (setq-default tab-width 2)
 
-(;; Automatically update changed buffer
- global-auto-revert-mode 1)
+(global-auto-revert-mode 1)  ; Automatically update changed buffer
 
-(progn
-  ;; Highlight matching brackets
+(progn  ; Highlight matching brackets
   (show-paren-mode)
   (set-face-background 'show-paren-match (face-background 'default))
   (set-face-foreground 'show-paren-match "#def")
   (set-face-attribute 'show-paren-match nil :weight 'extra-bold))
 
-(use-package magit
-  ;; Git package
+(use-package magit ; Git package
   :ensure t)
 
-(use-package expand-region
-  ;; Magic that I saw once.
+(use-package expand-region ; Magic that I saw once.
   :ensure t
   :config
   (evil-define-key 'normal 'global "q" #'er/expand-region)
   (evil-define-key 'normal 'global "Q" #'er/contract-region))
 
-(use-package popup-kill-ring
-  ;; Use this to yank multiple things.
-  :ensure t
-  :bind ("M-y" . popup-kill-ring))
-
-(use-package evil-numbers
-  ;; Vim Numbering
+(use-package popup-kill-ring ; Use this to yank multiple things.
   :ensure t
   :config
-  (global-set-key (kbd "C-=") 'evil-numbers/inc-at-pt)
-  (global-set-key (kbd "C--") 'evil-numbers/dec-at-pt))
+  (evil-define-key 'normal 'global "P" #'popup-kill-ring))
 
-(add-hook
- ;; Remove completion buffer when done
+(add-hook  ; Remove completion buffer when done
  'minibuffer-exit-hook
  '(lambda ()
     (let ((buffer "*Completions*"))
-      (and (get-buffer buffer)
-         (kill-buffer buffer)))))
+      (and (get-buffer buffer) (kill-buffer buffer)))))
 
-(; Save current desktop configs on exit
- desktop-save-mode 1)
+(desktop-save-mode 1)  ; Save current desktop configs on exit
 
-(;; Please don't ask whether to kill running processes
- add-hook 'comint-exec-hook
- (lambda () (set-process-query-on-exit-flag
-             (get-buffer-process (current-buffer)) nil)))
+(add-hook 'comint-exec-hook  ; Please don't ask whether to kill running processes
+          (lambda () (set-process-query-on-exit-flag
+                      (get-buffer-process (current-buffer)) nil)))
 
-(setq
- ;; stop making backup files
+(setq  ; stop making backup files
  make-backup-files nil
  auto-save-default nil)
 
-(use-package math-symbol-lists
+(use-package math-symbol-lists  ; Unicode symbols
   :ensure t
   :config
   (quail-define-package "math" "UTF-8" "Ω" t)
@@ -330,12 +275,10 @@
   ;; The fonts are: mscr (script), mbfscr (bold script), mfrak (frankfurt), mbf (boldface), Bbb (Double stroke)
   )
 
-(add-hook
- ;; Delete trailing whitespaces on save.
+(add-hook ; Delete trailing whitespaces on save.
  'before-save-hook 'delete-trailing-whitespace)
 
-;;; Fix lisp indent
-(let ((sif 'scheme-indent-function))
+(let ((sif 'scheme-indent-function))  ; Fix lisp indent
   (put 'set!           sif 1)
   (put 'match          sif 1)
   (put 'match*         sif 1)
@@ -356,16 +299,14 @@
   (put 'with-handlers  sif 1)
   (put 'while          sif 1)
   (put 'place          sif 1)
-  (put 'trace-lambda   sif 'defun) 
-  (put 'trace-define   sif 1) 
+  (put 'trace-lambda   sif 'defun)
+  (put 'trace-define   sif 1)
   (put 'with-syntax    sif 1))
 
-(defun gk-display-buffer-please-no (buf &rest ignore)
-  (error "Inhibited buffer: %s" (buffer-name buf)))
-(setq display-buffer-alist
-      `('("\\*Quail Completions" . (gk-display-buffer-please-no)) . ,display-buffer-alist))
-
 ;;; Custom functions
+(defun config () (interactive) (find-file "~/.emacs.d/init.el"))
+(defun thought () (interactive) (find-file "~/note/thought.txt"))
+
 (defun deGreek ()
   ;; deGreek: at least I know how to Emacs Lisp!. You can start with either, and the other one will finish the job.
   (interactive)
@@ -407,7 +348,7 @@
   (sit-for 1)
   (shell-command "xviewer ~/note/graph.svg"))
 
-;; Binary search: zingming.wordpress.com
+;; Binary search
 (defun line-number ()
   (string-to-number (format-mode-line "%l")))
 (defun line-count ()
@@ -431,7 +372,7 @@
   (evil-next-line (/ (- bin-upper bin-lower) 2)))
 
 ;; Pro lisp movements
-(setq evil-move-beyond-eol t)           ; The magic is here
+(setq evil-move-beyond-eol t) ; The magic is here
 (progn
   (evil-define-key 'normal 'global (kbd "M-h") 'backward-sexp)
   (evil-define-key 'normal 'global (kbd "M-l") 'forward-sexp)
@@ -456,7 +397,7 @@
  '(custom-enabled-themes (quote (adwaita)))
  '(package-selected-packages
    (quote
-    (lisp disable-mouse math-symbol-lists rainbow-identifiers spaceline avy smex ido-vertical-mode beacon evil-numbers evil-lion evil-commentary rainbow-delimiters linum-relative evil-surround evil use-package))))
+    (fold-this lisp disable-mouse math-symbol-lists rainbow-identifiers spaceline avy smex ido-vertical-mode beacon evil-numbers evil-lion evil-commentary rainbow-delimiters linum-relative evil-surround evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
